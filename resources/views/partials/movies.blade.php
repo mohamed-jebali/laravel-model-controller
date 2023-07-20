671302bc55e3e4a51d2336626a4eac22b3b381b5
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-07-20 17:25:54 +0200

    added cards

--- resources/views/partials/movies.blade.php
+++ resources/views/partials/movies.blade.php
@@ -1,16 +1,43 @@
+@extends ('layouts.app')
 
-<div class="container">
-    @foreach($movies as $movie)
-    <div class="card" style="width: 18rem;">
-      <div class="card-header">
-      {{ $movie->title }}
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">{{ $movie->original_title }}</li>
-        <li class="list-group-item">{{ $movie->nationality }}</li>
-        <li class="list-group-item">{{ $movie->date }}</li>
-        <li class="list-group-item">{{ $movie->vote }}</li>
-      </ul>
+@section("title","Movies")
+
+
+<div class="container mx-auto mt-4">
+    <div class="row justify-content-between">
+        @foreach($movies as $movie)
+        <div class="card px-0 col-2 mx-1 mb-4">
+          <div class="card-header">
+            <strong>
+            Title :
+            </strong> {{ $movie->title }}
+          </div>
+          <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+            <strong>
+            Original Title :
+            </strong>{{ $movie->original_title }}
+        </li>
+        <li class="list-group-item">
+            <strong>
+            Nationality :
+            </strong>
+            {{ $movie->nationality }}
+        </li>
+        <li class="list-group-item">
+        <strong>
+            Date :
+        </strong>
+                {{ $movie->date }}
+        </li>
+        <li class="list-group-item">
+        <strong>
+            Vote :
+        </strong>
+                {{ $movie->vote }}
+        </li>
+          </ul>
+        </div>
+        @endforeach
     </div>
-    @endforeach
 </div>
